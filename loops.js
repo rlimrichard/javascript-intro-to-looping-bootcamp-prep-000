c8ed51595bac2bb552cafa3cfc8193c08cb0c9e0
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-21 19:32:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,7 +8,7 @@ function doWhileLoop (array){
 
 
 function forLoop (array){
-    for (let i=0; i<25 ; i++){
+    for (var i=0; i<25 ; i++){
         array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
     }
     return array
