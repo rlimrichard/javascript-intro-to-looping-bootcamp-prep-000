13b0deddd2b124d3178dedac016a54ac60b645fa
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-21 19:30:35 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,28 @@
+function doWhileLoop (array){
+    while (maybeTrue() && array.length >0)
+        array.pop()
+
+    return array
+}
+
+
+function forLoop(array){
+    for (let i=0; i<25 ; i++){
+        array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+    }
+    return array
+}
+
+function whileLoop(n){
+    while (n > 0) {
+        console.log(--n)
+
+    return 'done';
+}
+
+
+
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
